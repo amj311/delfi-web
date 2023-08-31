c20489a4d8ff8ce5bef8f26d6b3bf98c0eda555b
Arthur Judd <amjudd315@gmail.com>
2023-08-30 21:18:57 -0600

    move delfi-core up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { accounts, scheduledTransactions } from '../delfi-core/dummyData';
+import { accounts, scheduledTransactions } from '../../delfi-core/dummyData';
 </script>
 
 <template>
